8e8bdc18c317988f1ad171b453fdd947595560df
sfia-o <sofia.vl.oliveira@gmail.com>
2023-04-19 23:52:18 +0000

    Using functions to determine boolean values of numbers

---
+++ isOdd.js
@@ -0,0 +1,8 @@
+const isOdd = function(num){
+  return num % 2 !== 0;
+
+}
+console.log("9 is odd: " + isOdd(9));
+console.log("23 is odd: " + isOdd(23));
+console.log("15 is odd: " + isOdd(15));
+console.log("4 is odd: " + isOdd(4));
