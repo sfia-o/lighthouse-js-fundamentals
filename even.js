9dca871bdb81843d659357da275e53abdd3c64af
sfia-o <sofia.vl.oliveira@gmail.com>
2023-04-19 23:38:47 +0000

    Using isEven function to return Boolean values about numbers

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven = function (num){
+  return num % 2 === 0
+}
+
+/*
+const tenIsEven = isEven(10);
+const thirteenIsEven = isEven(13);
+
+console.log(tenIsEven);
+console.log(thirteenIsEven);
+*/
+
+console.log(isEven(10));
+console.log(isEven(23));
